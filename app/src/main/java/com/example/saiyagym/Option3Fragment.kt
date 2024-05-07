f39cc60647554cde43c1237bad458fdfc43349db
AlejandroViewNext <alejandro.morenog@viewnext.com>
2024-05-07 20:45:43 +0200

    funcionalidad a cambiar contraseña

--- app/src/main/java/com/example/saiyagym/Option3Fragment.kt
+++ app/src/main/java/com/example/saiyagym/Option3Fragment.kt
@@ -1,5 +1,7 @@
 package com.example.saiyagym
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
@@ -7,12 +9,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.EditText
 import android.widget.Switch
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+
 class Option3Fragment : Fragment() {
 
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,30 +27,26 @@ class Option3Fragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_option3, container, false)
 
-
         val closeButton = view.findViewById<MaterialButton>(R.id.CerrarSesion)
 
-
         closeButton.setOnClickListener {
-
             val sharedPreferences = requireActivity().getSharedPreferences("my_preferences", Context.MODE_PRIVATE)
-
-
             val editor = sharedPreferences.edit()
             editor.clear().apply()
             requireActivity().finish()
-           }
+        }
+
+        val switch: Switch = view.findViewById(R.id.switchTema)
+
         val principal = activity as Principal
         val sp = principal.getSharedPreferences("my_preferences", MODE_PRIVATE)
         val editor = sp.edit()
-        val switch: Switch = view.findViewById(R.id.switchTema)
+
 
         val theme = sp.getInt("theme", 1)
-        if(theme==1){
-            switch.setChecked( false)
-        }else{
-            switch.setChecked( true)
-        }
+        switch.isChecked = theme != 1
+
+
         switch.setOnClickListener {
             if (switch.isChecked) {
                 editor.putInt("theme", 0)
@@ -54,8 +57,61 @@ class Option3Fragment : Fragment() {
             principal.setDayNight()
         }
 
+        val changePasswordButton = view.findViewById<MaterialButton>(R.id.button2)
 
+        changePasswordButton.setOnClickListener {
+            showChangePasswordDialog()
+        }
 
         return view
+    }
+
+    private fun showChangePasswordDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Cambiar Contraseña")
+
+        val view = layoutInflater.inflate(R.layout.dialog_change_password, null)
+        val currentPasswordEditText = view.findViewById<EditText>(R.id.currentPasswordEditText)
+        val newPasswordEditText = view.findViewById<EditText>(R.id.newPasswordEditText)
+
+        builder.setView(view)
+
+        builder.setPositiveButton("Cambiar") { _, _ ->
+            val currentPassword = currentPasswordEditText.text.toString()
+            val newPassword = newPasswordEditText.text.toString()
+
+            changePassword(currentPassword, newPassword)
+        }
+        builder.setNegativeButton("Cancelar") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+    private fun changePassword(currentPassword: String, newPassword: String) {
+        val user = FirebaseAuth.getInstance().currentUser
+        val credential = EmailAuthProvider.getCredential(user!!.email!!, currentPassword)
+
+
+        user.reauthenticate(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+
+                    user.updatePassword(newPassword)
+                        .addOnCompleteListener { updateTask ->
+                            if (updateTask.isSuccessful) {
+                                showToast("Contraseña actualizada con éxito")
+                            } else {
+                                showToast("Error al actualizar la contraseña")
+                            }
+                        }
+                } else {
+                    showToast("La contraseña actual es incorrecta")
+                }
+            }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
