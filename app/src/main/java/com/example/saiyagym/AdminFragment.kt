e4c6ff01ff1bac69c989de9c5152a38ade740d9d
AlejandroViewNext <alejandro.morenog@viewnext.com>
2024-05-14 21:21:08 +0200

    admin webView(chapuza)

--- app/src/main/java/com/example/saiyagym/AdminFragment.kt
+++ app/src/main/java/com/example/saiyagym/AdminFragment.kt
@@ -4,12 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 
 class AdminFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflar el diseño del fragmento
-        return inflater.inflate(R.layout.fragment_admin, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_admin, container, false)
+
+        // Obtener la referencia de la WebView desde el layout
+        val webView: WebView = rootView.findViewById(R.id.webView)
+
+        // Configurar el cliente de WebView para cargar en la misma WebView
+        webView.webViewClient = WebViewClient()
+
+        // Habilitar la configuración de JavaScript para la WebView (si es necesario)
+        webView.settings.javaScriptEnabled = true
+
+        // Cargar la URL de la tabla de usuarios de Firebase Authentication
+        webView.loadUrl("https://console.firebase.google.com/project/saiyagym-9000/authentication/users?hl=es")
+
+        return rootView
     }
 }
